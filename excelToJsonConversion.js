b64a0af9566a3dc53b5fa1256db3ef5d3f1d57eb
Alisha Virani <alisha.virani@bookmyshow.com>
2018-07-19 12:07:14 +0530

    Bug fixes

--- excelToJsonConversion.js
+++ excelToJsonConversion.js
@@ -18,26 +18,42 @@ const result = excelToJson({
 // console.log("result???", result.Sheet1);
 let data = result.Sheet1;
 let resultArray = [];
-
 for(let i = 1; i < data.length; i++) {
     if(resultArray.length) {
         for(let j = 0; j < resultArray.length; j++) {
 
             if(resultArray[j].name == data[i].Department) {
-                
 
                 for(let t = 0; t < resultArray[j].tribes.length; t++) {
                     
                     if(resultArray[j].tribes[t].name == data[i].Tribe) {
+                        console.log("heyyy", j);
                         resultArray[j].tribes[t].squads.push({
                             name: data[i].Squads,
                             PO: data[i].PO,
                             TO: data[i].TO,
                             TL: data[i].TL
-                        })
+                        });
+                        break;
                     }
-                    break;
+                    if(t == resultArray[j].tribes.length - 1) {
+                        addNewTribe();
+                        break;
+                    }
                 }
+
+                function addNewTribe() {
+                    resultArray[j].tribes.push({
+                        name: data[i].Tribe,
+                        squads: [{
+                            name: data[i].Squads,
+                            PO: data[i].PO,
+                            TO: data[i].TO,
+                            TL: data[i].TL,
+                        }]
+                    })
+                }
+
             } else {
                 resultArray.push({
                     name: data[i].Department,
@@ -70,6 +86,6 @@ for(let i = 1; i < data.length; i++) {
     }
 }
 
-console.log("resultArray??", resultArray);
+// console.log("resultArray??", resultArray);
 
 fs.writeFileSync('./data.json', JSON.stringify(resultArray, null, 2) , 'utf-8');
